c3fbf6b859a6d5c7710aea14433cc4c87a3f7f6c
Thomas van der Velde <contact@toolbarthomas.net>
2018-02-19 12:57:46 +0100

    Remove tmp directory

--- gulp/clean.js
+++ gulp/clean.js
@@ -2,10 +2,11 @@ module.exports = (GULP, GULP_PLUGINS, NODE_MODULES, REVISION) => {
     return function (callback) {
 
         return NODE_MODULES.del([
+            './.tmp',
             process.env.TIPICSS_DIST + '/main',
             process.env.TIPICSS_DIST + '/modules',
             process.env.TIPICSS_DIST + '/groups',
-            process.env.TIPICSS_DIST + '/templates'
+            process.env.TIPICSS_DIST + '/templates',
         ]);
 
     };
